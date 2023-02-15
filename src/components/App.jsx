3284375c346585330dfc9f70d881947be97651a6
Dranyk <dvdranyk@gmail.com>
2023-02-15 19:52:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Header from './Header/Header';
-// import HomePage from 'pages/HomePage/HomePage';
-// import MoviesPage from 'pages/MoviesPage/MoviesPage';
-import MovieDetails from '../pages/MovieDatails/MovieDetails';
-// import Reviews from './Reviews/Reviews';
-// import Cast from './Cast/Cast';
+
 
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
-
 const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
-
+const MovieDetails = lazy(() => import('pages/MovieDatails/MovieDetails'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
-const Cast = lazy(() => import('./Cast/Cast' /* webpackChunkName: "Cast" */));
+const Cast = lazy(() => import('./Cast/Cast'));
 
 export const App = () => {
   return (
